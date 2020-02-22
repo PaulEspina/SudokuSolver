4dbf41a6aef0855fc2d11d6aac7fa307634db070
Paul Kristopher Espina <kristopherespina@gmail.com>
2020-02-23 02:06:57 +0800

    made it!

--- SudokuSolver/Main.cpp
+++ SudokuSolver/Main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 #include "Tools.h"
 
@@ -7,7 +8,7 @@ using namespace std;
 
 bool Enter(char digits[9][9]);
 bool Load(char digits[9][9]);
-void Solve(const char digits[9][9]);
+void Solve(char digits[9][9]);
 
 int main()
 {
@@ -39,6 +40,16 @@ int main()
 		}
 		cout << endl;
 	}
+	cout << endl;
+	Solve(digits);
+	for(int i = 0; i < 9; i++)
+	{
+		for(int j = 0; j < 9; j++)
+		{
+			cout << digits[i][j];
+		}
+		cout << endl;
+	}
 	system("pause > 0");
 }
 
@@ -56,13 +67,6 @@ bool Load(char digits[9][9])
 	ifstream load(path);
 	if(load.is_open())
 	{
-		/*for(int i = 0; i < 9; i++)
-		{
-			for(int j = 0; j < 9; j++)
-			{
-				digits[i][j] = load.get();
-			}
-		}*/
 		int counter = 0;
 		for(char c = load.get(); load.good(); c = load.get())
 		{
@@ -72,7 +76,7 @@ bool Load(char digits[9][9])
 			counter++;
 		}
 		bool bad = false;
-		char valid_data[] = "123456789x";
+		char valid_data[] = "1234567890";
 		for(int i = 0; i < 9; i++)
 		{
 			for(int j = 0; j < 9; j++)
@@ -92,6 +96,7 @@ bool Load(char digits[9][9])
 		if(bad || load.bad())
 		{
 			cout << "Invalid file data.\n";
+			load.close();
 			return 0;
 		}
 	}
@@ -104,7 +109,97 @@ bool Load(char digits[9][9])
 	return 1;
 }
 
-void Solve(const char digits[9][9])
+void Solve(char digits[9][9])
 {
+	// Put hints to their respective bounds
+	vector<char> h_bounds[9], v_bounds[9], box_bounds[9];
+	for(int i = 0; i < 9; i++)
+	{
+		for(int j = 0; j < 9; j++)
+		{
 
+			if(digits[i][j] != '0')
+			{
+				h_bounds[i].push_back(digits[i][j]); // adds to horizontal bounds
+			}
+			if(digits[j][i] != '0')
+			{
+				v_bounds[i].push_back(digits[j][i]); // adds to vertival bounds
+			}
+			if(digits[j + i / 3][i / 3 + (j % 3) * 3] != '0') // adds to box bounds
+			{
+				box_bounds[i].push_back(digits[j + i / 3][i / 3 + (j % 3) * 3]);
+			}
+		}
+	}
+	char temp[9][9];
+	for(int i = 0; i < 9; i++)
+	{
+		for(int j = 0; j < 9; j++)
+		{
+			if(digits[i][j] != '0')
+			{
+				temp[i][j] = digits[i][j];
+			}
+		}
+	}
+	for(int i = 0; i < 9; i++)
+	{
+		for(int j = 0; j < 9; j++)
+		{
+			if(digits[i][j] == '0')
+			{
+				bool good;
+				int guess = 48;
+				do
+				{
+					good = true;
+					if(guess >= 57)
+					{
+						good = false;
+					}
+					else
+					{
+						guess++;
+					}
+					for(int k = 0; k < 9; k++ && good)
+					{
+						if(temp[i][k] == guess || temp[k][j] == guess || temp[i - i % 3 + k / 3][j - j % 3 + k % 3] == guess)
+						{
+							good = false;
+							break;
+						}
+					}
+					if(!good && guess >= 57)
+					{
+						temp[i][j] = '0';
+						do
+						{
+							if(j > 0)
+							{
+								j--;
+							}
+							else if(i > 0 && j == 0)
+							{
+								i--;
+								j = 8;
+							}
+							guess = temp[i][j];
+						}
+						while(digits[i][j] != '0');
+					}
+				}
+				while(!good);
+				temp[i][j] = guess;
+			}
+		}
+	}
+	
+	for(int i = 0; i < 9; i++)
+	{
+		for(int j = 0; j < 9; j++)
+		{
+			digits[i][j] = temp[i][j];
+		}
+	}
 }
